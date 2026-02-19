86ca156198c0eb28c85f810778e2397c3b223ae5
Andy Anderson <andy@clubanderson.com>
2026-02-19 13:49:42 -0500

    🐛 Restrict rewards to console repos only (#1208)
    Change default GitHub rewards scope from all repos in kubestellar/llm-d
    orgs to only kubestellar/console and kubestellar/console-marketplace.
    Coins should only be awarded for console-related contributions.
    
    Signed-off-by: Andrew Anderson <andy@clubanderson.com>

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -807,7 +807,7 @@ func LoadConfigFromEnv() Config {
 		FeedbackRepoOwner:   getEnvOrDefault("FEEDBACK_REPO_OWNER", "kubestellar"),
 		FeedbackRepoName:    getEnvOrDefault("FEEDBACK_REPO_NAME", "console"),
 		// GitHub activity rewards
-		RewardsGitHubOrgs: getEnvOrDefault("REWARDS_GITHUB_ORGS", "org:kubestellar org:llm-d"),
+		RewardsGitHubOrgs: getEnvOrDefault("REWARDS_GITHUB_ORGS", "repo:kubestellar/console repo:kubestellar/console-marketplace"),
 		// Skip onboarding questionnaire for new users
 		SkipOnboarding: os.Getenv("SKIP_ONBOARDING") == "true",
 		// Benchmark data from Google Drive
